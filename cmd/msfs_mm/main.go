40615658f5d81cdb0c9bea6b799bc1a1db0a727e
Frank Kopp <frank@familie-kopp.de>
2021-05-26 12:48:47 +0200

    Initial commit

---
+++ cmd/msfs_mm/main.go
@@ -0,0 +1,16 @@
+
+package main
+
+import (
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+)
+
+var (
+	out = message.NewPrinter(language.German)
+)
+
+func main() {
+	out.Println("Hello World")
+	
+}
